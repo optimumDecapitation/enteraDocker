cee0ec4f02cab8a4c44f2ead1b56513ea96df496
optimumdecapitation <jtmoneyfosho@gmail.com>
2019-12-02 17:35:49 -0500

    modified directory structure for build stage of deployment

--- buildAndLaunchStack.sh
+++ buildAndLaunchStack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "building custom images"
-docker build -t entera/prometheus ./prometheusentera
-docker build -t entera/grafana ./enteragrafana
-docker build -t entera/rubyloop ./rubyDockerentera
+docker build -t entera/prometheus ./enteraDocker/prometheusentera
+docker build -t entera/grafana ./enteraDocker/enteragrafana
+docker build -t entera/rubyloop ./enteraDocker/rubyDockerentera
 echo "launching stack"
 docker-compose up
