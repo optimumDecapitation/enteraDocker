38f3ef7abf52b3b3b0ce895ff4119e0806fe0dc5
optimumdecapitation <jtmoneyfosho@gmail.com>
2019-12-03 22:14:40 -0500

    modified flags once again

--- buildAndLaunchStack.sh
+++ buildAndLaunchStack.sh
@@ -4,4 +4,5 @@ docker build -t entera/prometheus ./enteraDocker/prometheusentera
 docker build -t entera/grafana ./enteraDocker/enteragrafana
 docker build -t entera/rubyloop ./enteraDocker/rubyDockerentera
 echo "launching stack"
-sudo docker-compose -d -f /home/ubuntu/enteraDocker/docker-compose.yml up
+sudo docker-compose -f /home/ubuntu/enteraDocker/docker-compose.yml up -d
+#sudo docker-compose -f ~/Desktop/enteraRepot/enteraDocker/docker-compose.yml up -d
